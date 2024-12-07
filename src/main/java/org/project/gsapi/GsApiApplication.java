419fd2cb079dfd3495a7b9131bc0f8456189d918
thanhhuyvochau <thanhhuyvochau@gmail.com>
2024-12-07 14:12:31 +0700

    first commit

---
+++ src/main/java/org/project/gsapi/GsApiApplication.java
@@ -0,0 +1,13 @@
+package org.project.gsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/gsapi/GsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.gsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
